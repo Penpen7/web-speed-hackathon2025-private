5d3c0ca33f68c72a50d7c5976a361411ef36f537
Penpen7 <Raosys@live.jp>
2025-03-22 18:46:10 +0900

    修正

--- workspaces/client/src/main.tsx
+++ workspaces/client/src/main.tsx
@@ -2,31 +2,35 @@ import '@wsh-2025/client/src/setups/polyfills';
 import '@wsh-2025/client/src/setups/luxon';
 import '@wsh-2025/client/src/setups/unocss';
 
-import { StrictMode } from 'react';
-import { hydrateRoot } from 'react-dom/client';
-import { createBrowserRouter, HydrationState, RouterProvider } from 'react-router';
+import {StrictMode} from 'react';
+import {createBrowserRouter, RouterProvider} from 'react-router';
 
-import { StoreProvider } from '@wsh-2025/client/src/app/StoreContext';
-import { createRoutes } from '@wsh-2025/client/src/app/createRoutes';
-import { createStore } from '@wsh-2025/client/src/app/createStore';
+import {StoreProvider} from '@wsh-2025/client/src/app/StoreContext';
+import {createRoutes} from '@wsh-2025/client/src/app/createRoutes';
+import {createRoot} from 'react-dom/client';
+import {createStore} from '@wsh-2025/client/src/app/createStore';
 
-declare global {
-  var __zustandHydrationData: unknown;
-  var __staticRouterHydrationData: HydrationState;
-}
-
-function main() {
-  const store = createStore({});
+const App = () => {
+  const store = createStore();
   const router = createBrowserRouter(createRoutes(store), {});
 
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <StoreProvider createStore={() => store}>
-        <RouterProvider router={router} />
-      </StoreProvider>
-    </StrictMode>,
+  return (
+    <StoreProvider createStore={() => store}>
+      <RouterProvider router={router} />
+    </StoreProvider>
   );
 }
 
-document.addEventListener('DOMContentLoaded', main);
+function main() {
+  const root = document.getElementById('root');
+  if (!root) {
+    throw new Error('Root element not found');
+  }
+
+  createRoot(root).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+}
+main();
