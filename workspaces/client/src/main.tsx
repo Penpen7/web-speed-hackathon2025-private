535dbf063107fc4bd2053819b898b61dacb86fae
Penpen7 <Raosys@live.jp>
2025-03-23 11:12:21 +0900

    documentにrenderするよう修正

--- workspaces/client/src/main.tsx
+++ workspaces/client/src/main.tsx
@@ -2,13 +2,13 @@ import '@wsh-2025/client/src/setups/polyfills';
 import '@wsh-2025/client/src/setups/luxon';
 import '@wsh-2025/client/src/setups/unocss';
 
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { createBrowserRouter, HydrationState, RouterProvider } from 'react-router';
+import {StrictMode} from 'react';
+import {createRoot} from 'react-dom/client';
+import {createBrowserRouter, HydrationState, RouterProvider} from 'react-router';
 
-import { StoreProvider } from '@wsh-2025/client/src/app/StoreContext';
-import { createRoutes } from '@wsh-2025/client/src/app/createRoutes';
-import { createStore } from '@wsh-2025/client/src/app/createStore';
+import {StoreProvider} from '@wsh-2025/client/src/app/StoreContext';
+import {createRoutes} from '@wsh-2025/client/src/app/createRoutes';
+import {createStore} from '@wsh-2025/client/src/app/createStore';
 
 declare global {
   var __zustandHydrationData: unknown;
@@ -18,12 +18,7 @@ declare global {
 function main() {
   const store = createStore({});
   const router = createBrowserRouter(createRoutes(store), {});
-  const root = document.getElementById('root');
-  if (!root) {
-    throw new Error('Root element not found');
-  }
-
-  createRoot(root).render(
+  createRoot(document).render(
     <StrictMode>
       <StoreProvider createStore={() => store}>
         <RouterProvider router={router} />
